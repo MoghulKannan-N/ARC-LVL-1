2ca7a9cbe4038d90ed2d8e74b2c07ac6d8827c7f
MoghulKannan-N <moghulkannann@gmail.com>
2025-11-26 01:11:02 +0530

    ble debug mode2

--- front end/teacher_dashboard/lib/config.dart
+++ front end/teacher_dashboard/lib/config.dart
@@ -3,7 +3,7 @@
 /// Global configuration file for backend endpoints
 
 // Base IP address for both Spring Boot and Flask servers
-const String flask = "https://fluffy-emus-allow.loca.lt";
+const String flask = "https://green-boats-bathe.loca.lt";
 
 
-const String sb = "https://moody-dolls-yawn.loca.lt";
+const String sb = "https://cyan-paths-film.loca.lt";
