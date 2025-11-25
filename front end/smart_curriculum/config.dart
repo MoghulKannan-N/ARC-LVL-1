8f97789cb297ab9a150e523143c4dd4294b862b1
MoghulKannan-N <moghulkannann@gmail.com>
2025-11-26 02:13:54 +0530

    remote1

---
+++ front end/smart_curriculum/config.dart
@@ -0,0 +1,9 @@
+// lib/config.dart
+
+/// Global configuration file for backend endpoints
+
+// Base IP address for both Spring Boot and Flask servers
+const String flask = "https://large-donuts-chew.loca.lt";
+
+
+const String sb = "https://honest-trams-slide.loca.lt";
--- front end/teacher_dashboard/lib/config.dart
+++ front end/teacher_dashboard/lib/config.dart
@@ -3,7 +3,7 @@
 /// Global configuration file for backend endpoints
 
 // Base IP address for both Spring Boot and Flask servers
-const String flask = "https://green-boats-bathe.loca.lt";
+const String flask = "https://large-donuts-chew.loca.lt";
 
 
-const String sb = "https://cyan-paths-film.loca.lt";
+const String sb = "https://honest-trams-slide.loca.lt";
