08148c6c8a42f850e31fdc7b6f1d5d03b0fa708f
MoghulKannan-N <moghulkannann@gmail.com>
2025-11-26 03:14:14 +0530

    cloudflaredtest

--- front end/smart_curriculum/config.dart
+++
@@ -1,9 +0,0 @@
-// lib/config.dart
-
-/// Global configuration file for backend endpoints
-
-// Base IP address for both Spring Boot and Flask servers
-const String flask = "https://presence-surprising-graph-cartoon.trycloudflare.com";
-
-
-const String sb = "hhttps://each-polymer-organizing-wine.trycloudflare.com";
--- front end/smart_curriculum/lib/config.dart
+++ front end/smart_curriculum/lib/config.dart
@@ -1,9 +1,5 @@
-// lib/config.dart
 
-/// Global configuration file for backend endpoints
-
-// Base IP address for both Spring Boot and Flask servers
-const String flask = "https://presence-surprising-graph-cartoon.trycloudflare.com";
+const String flask = "https://fighting-bunch-ink-harper.trycloudflare.com";
 
 
-const String sb = "hhttps://each-polymer-organizing-wine.trycloudflare.com";
+const String sb = "https://gods-alex-addition-sufficient.trycloudflare.com";
