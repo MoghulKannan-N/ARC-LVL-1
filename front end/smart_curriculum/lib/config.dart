3d944d592d93ee0b82fa22c4ba11aea895be9b43
dhanacr7 <dhanapriyan81@gmail.com>
2025-12-12 07:34:58 +0530

    base url is changed

--- front end/smart_curriculum/lib/config.dart
+++ front end/smart_curriculum/lib/config.dart
@@ -1,3 +1,3 @@
-const String flask ="https://spouse-closer-toolbar-sky.trycloudflare.com";
-const String sb ="https://feelings-kind-needed-brandon.trycloudflare.com";
-const String aiBase ="https://ebook-landscapes-processors-thoroughly.trycloudflare.com";
+const String flask ="https://jam-federal-www-puts.trycloudflare.com";
+const String sb ="https://without-home-area-wilderness.trycloudflare.com";
+const String aiBase ="https://thinking-adapted-accepted-dolls.trycloudflare.com";
