aa0d0d3909c072f85d716703284a516dcf258855
dhanacr7 <dhanapriyan81@gmail.com>
2025-12-12 07:45:32 +0530

    confi ble

--- front end/smart_curriculum/android/app/src/main/kotlin/com/example/bus_booking/MainActivity.kt
+++ front end/smart_curriculum/android/app/src/main/kotlin/com/example/bus_booking/MainActivity.kt
@@ -114,7 +114,7 @@ class MainActivity : FlutterActivity() {
 
         val settings = AdvertiseSettings.Builder()
             .setAdvertiseMode(AdvertiseSettings.ADVERTISE_MODE_LOW_LATENCY)
-            .setTxPowerLevel(AdvertiseSettings.ADVERTISE_TX_POWER_HIGH)
+            .setTxPowerLevel(AdvertiseSettings.ADVERTISE_TX_POWER_ULTRA_LOW)
             .setConnectable(false)
             .build()
 
@@ -264,7 +264,7 @@ class MainActivity : FlutterActivity() {
 
         val settings = AdvertiseSettings.Builder()
             .setAdvertiseMode(AdvertiseSettings.ADVERTISE_MODE_LOW_LATENCY)
-            .setTxPowerLevel(AdvertiseSettings.ADVERTISE_TX_POWER_HIGH)
+            .setTxPowerLevel(AdvertiseSettings.ADVERTISE_TX_POWER_ULTRA_LOW)
             .setConnectable(false)
             .build()
 
